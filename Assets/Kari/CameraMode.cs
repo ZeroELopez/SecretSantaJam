97332a6f60c567c8fad76e7e48b95eeab82a223c
ZeroELopez <60636555+ZeroELopez@users.noreply.github.com>
2023-12-04 17:29:01 -0500

    Update CameraMode.cs

--- Assets/Kari/CameraMode.cs
+++ Assets/Kari/CameraMode.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+//so the original script was made by me, Kari, but
+//Griffin edited it so that there is a point system
+//to the game
 public class CameraMode : MonoBehaviour
 {
+//p1 is normal platformer camera pos and 
+//p2 is the picture mode camera pos
     [SerializeField]Transform pos1;
     [SerializeField] Transform pos2;
 
+//speed of transition, have it be editable by designers
     [SerializeField] float transitionSpeed;
 
+//unused events that can be taken out and out into
+//event hub
     public UnityEvent onCameraOn;
     public UnityEvent onCameraOff;
 
+//event for taking a successful picture of a creature
     public UnityEvent snapTaken;
 
 
@@ -21,6 +30,8 @@ public class CameraMode : MonoBehaviour
     [SerializeField] float cameraReadyDistance = .5f;
 
     [SerializeField] Rect screenRect;
+
+//so 
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +42,8 @@ public class CameraMode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+//Check input from player. move camera if player is
+//in camera mode.
         cameraOn = Input.GetKey(KeyCode.LeftControl);
         PlayerMovement.still = cameraOn;
 
@@ -47,10 +59,14 @@ public class CameraMode : MonoBehaviour
         filter.useDepth = false;
     }
 
+
+//the logic for taking a snapshot
     public void TakeSnapshot()
     {
+//if camera is not in position then return with no picture 
         if (Vector3.Distance(transform.position, pos2.position) > cameraReadyDistance)
             return;
+
 
         int points = 0;
         foreach (Creature c in creatures)
