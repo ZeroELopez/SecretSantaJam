08e113af310b9001ec17b71b9cd144b718515eff
ZeroELopez <zeroelopez@yahoo.com>
2023-12-21 12:33:18 -0500

    UIFix2

--- Assets/UIScript.cs
+++ Assets/UIScript.cs
@@ -8,6 +8,10 @@ public class UIScript : MonoBehaviour
 {
     Controls playerControls;
     Button[] list;
+    [SerializeField] SpriteRenderer[] visualSprites;
+    [SerializeField] Image[] visualImages;
+    Sprite[] originalVisuals;
+
     int selected;
 
     // Start is called before the first frame update
@@ -17,6 +21,8 @@ public class UIScript : MonoBehaviour
         selected = 0;
         playerControls = new Controls();
         playerControls.Enable();
+
+        GrabDefaultValues();
 
         //Set up Input System callbacks;
         playerControls.Actions.Move.started += OnMove;
@@ -30,6 +36,17 @@ public class UIScript : MonoBehaviour
     {
         selected -= Mathf.CeilToInt(context.ReadValue<Vector2>().y);
         Mathf.Clamp(selected, 0, list.Length - 1);
+
+        UpdateVisuals();
+    }
+
+    void GrabDefaultValues()
+    {
+
+    }
+
+    void UpdateVisuals()
+    {
 
     }
 
